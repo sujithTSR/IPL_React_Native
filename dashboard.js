6ac079341efc2f4fb9657b96c32120e44dbbf7df
sujithTSR <sujitroyal.005@gmail.com>
2016-04-08 20:05:36 +0530

    Dashboard started

---
+++ dashboard.js
@@ -0,0 +1,153 @@
+'use strict';
+import React, {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Component,
+  Image,
+  ListView,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+var _ = require('lodash');
+class Dashboard extends React.Component{
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.first}>
+          <View style={styles.leftContainer}>
+            <Image
+              style={styles.thumbnail}
+              source={require('./ball.png')}
+            />
+          </View>
+
+          <View style={styles.rightContainer}>
+            <Image
+              style={styles.thumbnail}
+              source={require('./bat.png')}
+            />
+          </View>
+        </View>
+        <View style={styles.second}>
+          <View style={styles.leftContainer}>
+            <Image
+              style={styles.thumbnail}
+              source={require('./win.png')}
+            />
+          </View>
+
+          <View style={styles.rightContainer}>
+            <Image
+              style={styles.thumbnail}
+              source={require('./lost.png')}
+            />
+          </View>
+        </View>
+        <View style={styles.third}>
+          <Text>
+          Credits
+          </Text>
+        </View>
+        <View style={styles.fourth}>
+          <Text>
+          PLACE THE BET
+          </Text>
+        </View>
+
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingRight:20,
+    paddingBottom: 20,
+    paddingTop: 20,
+    shadowColor: '#ffff',
+    shadowOpacity: 1.2,
+    shadowRadius: 2,
+
+  },
+  first:{
+    flex: 1,
+    flexDirection: 'row',
+  },
+  second:{
+    flex: 1,
+    flexDirection: 'row',
+  },
+  third:{
+    flex: 1,
+    flexDirection: 'row',
+  },
+  fourth:{
+    flex: 1,
+    flexDirection: 'row',
+  },
+
+  leftContainer:{
+    padding:20,
+    flex:1,
+  },
+  rightContainer: {
+    padding: 20,
+    flex: 1,
+  },
+  starcontainer: {
+    flex:1,
+    padding:7,
+    flexDirection: 'row'
+  },
+  thumbnail: {
+    width: 100,
+    height: 100,
+  },
+  star:{
+    width: 10,
+    height: 10
+  },
+  play:{
+    width: 20,
+    height: 20,
+    marginLeft: 160
+  },
+  title: {
+    fontSize: 18,
+    marginLeft: 8,
+    textAlign: 'left'
+
+  },
+  year: {
+    marginLeft:8,
+    textAlign: 'left'
+  },
+
+  separator: {
+    height: 1,
+    backgroundColor: '#CCC',
+    shadowColor: '#FFFF',
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    shadowOffset:{
+      height:1,
+      width:0
+    }
+
+    },
+  listView: {
+    paddingTop: 10,
+    backgroundColor: '#F5FCFF'
+  }
+});
+
+
+
+module.exports = Dashboard;
--- index.android.js
+++ index.android.js
@@ -1,51 +1,128 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-
+'use strict';
 import React, {
   AppRegistry,
-  Component,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight,
+  AlertIOS,
+  Dimensions,
 } from 'react-native';
 
-class IPL_react_native extends Component {
+var {SKNavigator} = require('react-native-sk-navigator');
+var Dashboard = require('./dashboard.js')
+var Page = React.createClass({
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+      <View style={styles.page}>
+        {/* Second page render */}
+
+        <TouchableHighlight
+          style={[styles.button, styles.goback]}
+          underlayColor='#c8c7cc'
+          onPress={() => this.props.navigator.pop()}
+        >
+          <Text>go back</Text>
+        </TouchableHighlight>
       </View>
     );
   }
-}
+});
 
-const styles = StyleSheet.create({
+var Home = React.createClass({
+  showPage(type){
+      this.props.navigator.push({
+      type: type, // simple
+      component: Page,
+      passProps: {
+        navigator: this.props.navigator,
+        type: type
+      },
+    });
+  },
+
+  renderButton(type, i){
+    return (
+        <TouchableHighlight
+        key={i}  // First Button
+        style={styles.button}
+        underlayColor='#c8c7cc'
+        onPress={() => this.showPage(type)}
+        >
+      <Text>{type}</Text>
+      </TouchableHighlight>
+    )
+  },
+
+  render() {
+    var types = ['simple'];
+    return (
+      <View style={styles.container}>
+        <View style={styles.top}>
+          {types.map(this.renderButton)}
+        </View>
+        <View style={styles.bottom}>
+          <Dashboard />
+        </View>
+
+      </View>
+    );
+  }
+});
+
+
+var test = React.createClass({
+  render() {
+    return (
+      <SKNavigator
+        initialRoute={{
+           component: Home,
+           title: 'Home',
+           type: 'none',
+           passProps: {
+           }
+         }}/>
+    );
+  }
+});
+
+var styles = {
   container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  top:{
+    flex:1,
+  },
+  bottom: {
+    flex:1,
+  },
+  page:{
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  button: {
+    width: 250,
+    height: 20,
+    borderRadius: 5,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold'
   },
-});
+  desc: {
+    fontSize: 14,
+    color: 'green',
+  },
+  goback: {
+    marginTop: 10
+  }
+};
 
-AppRegistry.registerComponent('IPL_react_native', () => IPL_react_native);
+AppRegistry.registerComponent('IPL_react_native', () => test);
